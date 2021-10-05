247cc9b271c9a0be5e8b70fa858363c4f384e8b6
tatsuzaki98 <tatsuzaki98@gmail.com>
2021-10-06 00:07:31 +0900

    edit prompt color

--- configs/style.zsh
+++ configs/style.zsh
@@ -2,6 +2,6 @@
 autoload -U colors && colors
 # autoload -U compinit
 # compinit
-PROMPT="${fg[red]}%n@%m${reset_color} ${fg[green]}%~${reset_color}
+PROMPT="${fg[cyan]}%n@%m${reset_color} ${fg[green]}%~${reset_color}
 \$ "
 
