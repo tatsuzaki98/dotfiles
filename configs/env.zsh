d7f21ec92f9323d62a3c62a566892aa21a0878ee
tatsuzaki98 <tatsuzaki98@gmail.com>
2022-01-21 18:13:56 +0900

    edit pyenv setting

--- configs/env.zsh
+++ configs/env.zsh
@@ -3,9 +3,10 @@ printf "checking for \e[36mpyenv\e[m..."
 if [ -d "$HOME/.pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
+  # eval "$(pyenv init --path)"
+  # eval "$(pyenv init -)"
   printf " \e[32mok\e[m.\n"
 else
   printf "not detected.\n"
 fi
+
