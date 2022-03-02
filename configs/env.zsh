54de7ecf9993455a15f1cda1433cd93ea28a7f81
Matsuzaki Masato <tatsuzaki98@gmail.com>
2022-03-02 20:44:49 +0900

    add nvm

--- configs/env.zsh
+++ configs/env.zsh
@@ -10,3 +10,14 @@ else
   printf "not detected.\n"
 fi
 
+
+# nvm | Node.js version manager
+printf "checking for \e[36mnvm\e[m..."
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  printf " \e[32mok\e[m.\n"
+else
+  printf "not detected.\n"
+fi
--- configs/style.zsh
+++ configs/style.zsh
@@ -2,6 +2,6 @@
 autoload -U colors && colors
 # autoload -U compinit
 # compinit
-PROMPT="${fg[cyan]}%n@%m${reset_color} ${fg[green]}%~${reset_color}
+PROMPT="${fg[cyan]}%n@%m:${fg[green]}%~ ${reset_color}
 \$ "
 
