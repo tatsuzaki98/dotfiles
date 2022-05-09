ecd9760a0512064aec4c9a24bfb79ec1a76de6d0
tatsuzaki98 <tatsuzaki98@gmail.com>
2022-05-09 16:29:45 +0900

    activate oneapi and venv by alias.

--- configs/alias.zsh
+++ configs/alias.zsh
@@ -1,33 +1,6 @@
-# ls
-printf "checking for \e[36mexa\e[m..."
-if type exa > /dev/null; then
-  printf " \e[32mok\e[m.\n"
-  alias ls='exa --sort=extension --group-directories-first'
-else
-  printf "not detected.\n"
-  if [ "$(uname)" "==" "Darwin" ]; then
-    alias ls='ls -G'
-  else
-    alias ls='ls --color=auto'
-  fi
-fi
-alias ll='ls -al'
-alias la='ls -a'
-
-
 # grep
 if type ggrep > /dev/null; then
-  alias grep='ggrep --color -P'
+  alias grep='ggrep --color'
 else
   alias grep='grep --color=always'
-fi
-
-
-# cat
-printf "checking for \e[36mbat\e[m..."
-if type bat > /dev/null; then
-  printf " \e[32mok\e[m.\n"
-  alias cat='bat --style="plain" --theme="ansi"'
-else
-  printf "not detected.\n"
 fi
--- configs/env.zsh
+++ configs/env.zsh
@@ -1,5 +1,32 @@
+# exa 
+printf "\e[36mexa\e[m:"
+if type exa > /dev/null; then
+  printf " \e[32mok\e[m.\n"
+  alias ls='exa --sort=extension --group-directories-first --group'
+else
+  printf "not available.\n"
+  if [ "$(uname)" "==" "Darwin" ]; then
+    alias ls='ls -G'
+  else
+    alias ls='ls --color=auto'
+  fi
+fi
+alias ll='ls -al'
+alias la='ls -a'
+
+
+# bat
+printf "\e[36mbat\e[m:"
+if type bat > /dev/null; then
+  printf " \e[32mok\e[m.\n"
+  alias cat='bat --style="plain" --theme="ansi"'
+else
+  printf "not available.\n"
+fi
+
+
 # pyenv
-printf "checking for \e[36mpyenv\e[m..."
+printf "\e[36mpyenv\e[m:"
 if [ -d "$HOME/.pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
@@ -7,28 +34,31 @@ if [ -d "$HOME/.pyenv" ]; then
   # eval "$(pyenv init -)"
   printf " \e[32mok\e[m.\n"
 else
-  printf "not detected.\n"
+  printf "not available.\n"
 fi
 
 
 # nvm | Node.js version manager
-printf "checking for \e[36mnvm\e[m..."
-if [ -d "$HOME/.nvm" ]; then
+function __nvm-activate__() {
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  printf " \e[32mok\e[m.\n"
+}
+printf "\e[36mnvm\e[m:"
+if [ -d "$HOME/.nvm" ]; then
+  alias nvm-activate="__nvm-activate__"
+  printf " \e[32mnvm-activate\e[m\n"
 else
-  printf "not detected.\n"
+  printf "not available.\n"
 fi
 
 
 # intel oneapi
-printf "checking for \e[36moneapi\e[m..."
+printf "\e[36moneapi\e[m:"
 if [ -d "/opt/intel/oneapi" ]; then
-  . /opt/intel/oneapi/setvars.sh > /dev/null
-  printf " \e[32mok\e[m.\n"
+  alias oneapi-activate=". /opt/intel/oneapi/setvars.sh"
+  printf " \e[32moneapi-activate\e[m\n"
 else
-  printf "not detected.\n"
+  printf "not available.\n"
 fi
 
