74a1fbb800b3b50e049e39418ecb497897c21b69
tatsuzaki98 <tatsuzaki98@gmail.com>
2023-02-18 18:44:17 +0900

    edit git-prompt style

--- assets/style.zsh
+++ assets/style.zsh
@@ -7,8 +7,10 @@ setopt PROMPT_SUBST
 GIT_PS1_SHOWDIRTYSTATE="true"
 GIT_PS1_SHOWUNTRACKEDFILES="true"
 GIT_PS1_SHOWUPSTREAM="auto"
- GIT_PS1_STATESEPARATOR=" "
+GIT_PS1_STATESEPARATOR=""
 
-PROMPT='${fg[blue]}%n@%m:%~
-${fg[green]}$(__git_ps1 "%s")${reset_color}\$ '
+# name: %n
+# host: %m
+PROMPT='${fg[green]}$(__git_ps1 "(%s) ")%~ %n@%m
+${reset_color}\$ '
 
