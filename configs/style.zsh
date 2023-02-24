a6462c82370b0b490da43bd6fc33867a882f0c21
tatsuzaki98 <tatsuzaki98@gmail.com>
2023-02-24 12:32:20 +0900

    git promptのスタイル変更

--- configs/style.zsh
+++ configs/style.zsh
@@ -1,16 +1,13 @@
 # zsh appearance
-
-autoload -U colors && colors
-
 setopt PROMPT_SUBST
-
 GIT_PS1_SHOWDIRTYSTATE="true"
 GIT_PS1_SHOWUNTRACKEDFILES="true"
 GIT_PS1_SHOWUPSTREAM="auto"
-GIT_PS1_STATESEPARATOR=""
+GIT_PS1_STATESEPARATOR=":"
 
-# name: %n
-# host: %m
-PROMPT='${fg[green]}$(__git_ps1 "(%s) ")${fg[blue]}%~ ${fg[green]}%n@%m
-${reset_color}\$ '
+PROMPT_FIRST_LINE='\
+%F{green}[$(date +%H:%M:%S)] %n@%m:%~%f \
+%K{green}%F{black}$(__git_ps1 " %s ")%f%k\
+'
 
+PROMPT_SECOND_LINE='$ '
