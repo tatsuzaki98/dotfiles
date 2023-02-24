d274f474e18bf3a228cf73825082a1912ef3e7d5
tatsuzaki98 <tatsuzaki98@gmail.com>
2023-02-24 12:46:59 +0900

    style変更

--- configs/style.zsh
+++ configs/style.zsh
@@ -6,8 +6,14 @@ GIT_PS1_SHOWUPSTREAM="auto"
 GIT_PS1_STATESEPARATOR=":"
 
 PROMPT_FIRST_LINE='\
-%F{green}[$(date +%H:%M:%S)] %n@%m:%~%f \
-%K{green}%F{black}$(__git_ps1 " %s ")%f%k\
+%F{green}%n@%m:%~%f\
 '
 
-PROMPT_SECOND_LINE='$ '
+PROMPT_SECOND_LINE='\
+%K{green}%F{black} $(date +%H:%M:%S) $(__git_ps1 "%s ")%f%k\
+ $ \
+'
+
+PROMPT="\
+$PROMPT_FIRST_LINE
+$PROMPT_SECOND_LINE"
