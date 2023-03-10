f043b84f96798769490ac136bd75ea587cb62b6c
tatsuzaki98 <tatsuzaki98@gmail.com>
2023-03-10 15:54:48 +0900

    change `docker-compose` to `docker compose`

--- configs/alias.zsh
+++ configs/alias.zsh
@@ -5,12 +5,12 @@ else
   alias grep='grep --color=always'
 fi
 
-# docker-compose
-alias dc='docker-compose'
-alias dc-build='docker-compose build'
-alias dc-run='docker-compose run'
-alias dc-up='docker-compose up'
-alias dc-down='docker-compose down'
-alias dc-logs='docker-compose logs'
-alias dc-ps='docker-compose ps'
-alias dc-exec='docker-compose exec'
+# docker compose
+alias dc='docker compose'
+alias dc-build='docker compose build'
+alias dc-run='docker compose run'
+alias dc-up='docker compose up'
+alias dc-down='docker compose down'
+alias dc-logs='docker compose logs'
+alias dc-ps='docker compose ps'
+alias dc-exec='docker compose exec'
