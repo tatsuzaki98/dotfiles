baaf7f244f469471515ebd81318f266f5ebf5a29
tatsuzaki98 <tatsuzaki98@gmail.com>
2024-01-13 15:10:00 +0900

    pyenvを編集

--- configs/env.zsh
+++ configs/env.zsh
@@ -29,9 +29,8 @@ fi
 printf "\e[36mpyenv\e[m:"
 if [ -d "$HOME/.pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  # eval "$(pyenv init --path)"
-  # eval "$(pyenv init -)"
+  [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
   printf " \e[32mok\e[m.\n"
 else
   printf "not available.\n"
