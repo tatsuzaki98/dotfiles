53b21f3270664faa9ff1e0ee4abbfad2ce5f6b81
tatsuzaki98 <tatsuzaki98@gmail.com>
2024-04-16 15:00:48 +0900

    update pyenv config

--- configs/env.zsh
+++ configs/env.zsh
@@ -29,9 +29,8 @@ fi
 printf "\e[36mpyenv\e[m:"
 if [ -d "$HOME/.pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  # eval "$(pyenv init --path)"
-  # eval "$(pyenv init -)"
+  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
   printf " \e[32mok\e[m.\n"
 else
   printf "not available.\n"
