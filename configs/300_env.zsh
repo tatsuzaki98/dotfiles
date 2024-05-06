2eee4a3c30a266b5e16f363e5474cff3974a8c5c
tatsuzaki98 <tatsuzaki98@gmail.com>
2024-05-06 19:00:09 +0900

    スタートアップ時の表示を整理

--- configs/300_env.zsh
+++ configs/300_env.zsh
@@ -1,11 +1,24 @@
+#!/bin/zsh
+
+printf "        available:"
+
+function __print_ok() {
+  printf " \e[36m$1\e[m,"
+}
+
+function __print_not_available() {
+  printf " \e[31m$1\e[m,"
+}
+
+
 # pyenv
 if [ -d "$HOME/.pyenv" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
-  printf "        [\e[32mok\e[m] \e[36mpyenv\e[m\n"
+  __print_ok "pyenv"
 else
-  printf "        [\e[31mnot available\e[m] \e[36mpyenv\e[m\n"
+  __print_not_available "pyenv"
 fi
 
 
@@ -14,31 +27,33 @@ if [ -d "$HOME/.nvm" ]; then
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  printf "        [\e[32mok\e[m] \e[36mnvm\e[m\n"
+  __print_ok "nvm"
 else
-  printf "        [\e[31mnot available\e[m] \e[36mnvm\e[m\n"
+  __print_not_available "nvm"
 fi
 
 
 # cargo
 if type cargo > /dev/null; then
-  printf "        [\e[32mok\e[m] \e[36mcargo\e[m\n"
+  __print_ok "cargo"
 else
-  printf "        [\e[31mnot available\e[m] \e[36mcargo\e[m\n"
+  __print_not_available "cargo"
 fi
 
 
 # docker
 if type docker > /dev/null; then
-  printf "        [\e[32mok\e[m] \e[36mdocker\e[m\n"
+  __print_ok "docker"
 else
-  printf "        [\e[31mnot available\e[m] \e[36mdocker\e[m\n"
+  __print_not_available "docker"
 fi
 
 
 # ifort
 if type ifort > /dev/null; then
-  printf "        [\e[32mok\e[m] \e[36mifort\e[m\n"
+  __print_ok "ifort"
 else
-  printf "        [\e[31mnot available\e[m] \e[36mifort\e[m\n"
+  __print_not_available "ifort"
 fi
+
+printf "\n"
--- configs/310_alias.zsh
+++ configs/310_alias.zsh
@@ -1,31 +1,41 @@
+#!/bin/zsh
+
 # list of alias
 # - grep: ggrep or grep with color
 # - dc: docker compose, and some alias for docker compose command
 # - ls: exa if available
 #   - tree: exa --tree
 
+function __print_alias() {
+  printf "        alias: \e[36m$1\e[m = \e[36m$2\e[m\n"
+}
+
 # grep
 if type ggrep > /dev/null; then
   alias grep='ggrep --color'
+  __print_alias "grep" "ggrep"
 else
   alias grep='grep --color=always'
 fi
 
 # docker compose
-alias docker-compose='docker compose'
-alias dc='docker compose'
-alias dc-build='docker compose build'
-alias dc-run='docker compose run'
-alias dc-up='docker compose up'
-alias dc-down='docker compose down'
-alias dc-logs='docker compose logs'
-alias dc-ps='docker compose ps'
-alias dc-exec='docker compose exec'
+if type docker-compose > /dev/null; then
+  alias docker-compose='docker compose'
+  alias dc='docker compose'
+  alias dc-build='docker compose build'
+  alias dc-run='docker compose run'
+  alias dc-up='docker compose up'
+  alias dc-down='docker compose down'
+  alias dc-logs='docker compose logs'
+  alias dc-ps='docker compose ps'
+  alias dc-exec='docker compose exec'
+  __print_alias "dc" "docker-compose"
+fi
 
 # ls -> exa 
 if type exa > /dev/null; then
   alias ls='exa --sort=extension --group-directories-first --group'
-  printf "        alias: \e[36mls\e[m -> \e[36mexa\e[m\n"
+  __print_alias "ls" "exa"
 else
   if [ "$(uname)" "==" "Darwin" ]; then
     alias ls='ls -G'
@@ -39,6 +49,5 @@ alias la='ls -a'
 # tree
 if type exa > /dev/null; then
   alias tree='exa --tree'
-else
-  echo > /dev/null
+  __print_alias "tree" "exa --tree"
 fi
